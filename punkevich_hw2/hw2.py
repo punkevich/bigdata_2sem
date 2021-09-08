c9a147bfcb92d2ddf7a6a621f8239a49335816a2
punkevich <punkevich@list.ru>
2021-09-08 19:17:50 +0300

    add some

--- punkevich_hw2/hw2.py
+++ punkevich_hw2/hw2.py
@@ -7,10 +7,9 @@ from mrjob.compat import jobconf_from_env
 from itertools import product
 import csv
 import re
+import hashlib
 
 WORD_RE = re.compile(r"[\w']+")
-
-#print(distance.get_jaro_distance("Юнипро, акция обыкновенная (RU000A0JNGA5, UPRO)", "Мосэнерго", winkler=True, scaling=0.1))
 
 fieldnames = ['id','name','sector','price','volume','ticker','ebitda','net profit','capitalization']
 fields = 'id,name,ticker,cap,avgvolume,eps,price'
@@ -29,7 +28,6 @@ class MREntityResolution(MRJob):
                                 str1 = value1.split(',')
                                 str2 = value2.split(',')
                                 name1, name2 = str1[1], str2[1]
-                                #yield (docname1, docname2), (value1, value2, distance.get_jaro_distance(name1, name2, winkler=True, scaling=0.1))
                                 yield (docname1, docname2, value1, value2), distance.get_jaro_distance(name1, name2, winkler=True, scaling=0.1)
 
         def mapper2(self, key, metrics):
@@ -44,6 +42,30 @@ class MREntityResolution(MRJob):
                         jaro_dist = distance.get_jaro_distance(ticker1, ticker2, winkler=True, scaling=0.1)
                         yield (key), jaro_dist 
         
+        def mapper3(self, key, metrics):
+                obj1, obj2 = key[2], key[3]
+                price1, price2 = obj1.split(',')[6], obj2.split(',')[6]
+                key.append(metrics)
+                yield (key), (price1, price2)
+        
+        def reducer3(self, key, values):
+                arr = list(values)
+                for price1, price2 in arr:
+                        price_comparison = round(1 - abs(float(price1) - float(price2)))
+                        yield (key), price_comparison
+                        
+        def mapper4(self, key, price_comparison):
+                jd_name, jd_ticker = key[4], key[5]
+                yield (key[:4]), (jd_name, jd_ticker, price_comparison)
+                
+        def reducer4(self, key, metrics):
+                arr = list(metrics)
+                for jd_name, jd_ticker, price_comp in arr:
+                        result = ((float(jd_name) + float(jd_name)) / 2) * jd_name
+                if (result > 0.75 and result < 1.00):
+                        
+                yield key, result               
+        
         def steps(self):
         	return [
             		MRStep(
@@ -53,15 +75,15 @@ class MREntityResolution(MRJob):
             		MRStep(
             			mapper=self.mapper2,
             			reducer=self.reducer2,
-            			)
-            		#MRStep(
-			#	mapper=self.get_termscount,
-			#	reducer=self.get_tfidf,
-			#	),
-			#MRStep(
-			#	combiner=self.filter,
-			#	reducer=self.list_of_doc,
-			#	),
+            			),
+            		MRStep(
+				mapper=self.mapper3,
+				reducer=self.reducer3,
+				),
+			MRStep(
+				mapper=self.mapper4,
+				reducer=self.reducer4,
+				),
         	]
 
 
